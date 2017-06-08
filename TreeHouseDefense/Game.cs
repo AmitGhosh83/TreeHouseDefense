18fc54559059af4ea5168134408dfd9fb70e23a6
Amit Kumar Ghosh <amitghosh83@gmail.com>
2017-06-08 13:12:10 -0700

    Validation in contructor to check if the new Maplocation Object is within the periphery of the maps.

--- TreeHouseDefense/Game.cs
+++ TreeHouseDefense/Game.cs
@@ -12,8 +12,13 @@ namespace TreeHouseDefense
         {
 
             Map map = new Map(8,5);
+            Point point = new Point(2, 3);
+            Point point1 = new Point(0, 5);
 
+            bool IsOnMap = map.OnMap(new MapLocation(8,5));
 
+            Console.WriteLine("{0}", IsOnMap);
+            Console.WriteLine("{0}", point.DistanceTo(point1));
             Console.ReadKey();
         }
     }
